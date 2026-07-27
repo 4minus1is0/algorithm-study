66a1167d59f8a20b5dc522ea10ea5bfc78c241b8
yj9107v <yj9107v@gmail.com>
2026-07-27 09:37:38 +0900

    Same Tree

---
+++ yj9107v/binarytree/Same Tree.java
@@ -0,0 +1,29 @@
+/**
+ 날짜: 2026/07/27
+ 시간: 10분
+ 해결: O
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
