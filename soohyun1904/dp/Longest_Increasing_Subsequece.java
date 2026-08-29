241111979be8aa6d5dc030e5d2de67b63414e421
이수현 <dalsu4594@naver.com>
2026-08-29 18:12:37 +0900

    15주차 알고리즘 문제 풀이
    - DP 문제 풀이
    - Longest Increasing Subsequence

---
+++ soohyun1904/dp/Longest_Increasing_Subsequece.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1); 
+        int result = 1;
+        for(int i = 1; i <nums.length; i++) {
+            int max = dp[i];
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    max = Math.max(max, dp[j] + 1);
+                }
+            }
+            dp[i] = max;
+            result  = Math.max(result, dp[i]);
+        }
+
+        return result;
+    }
+}
+
